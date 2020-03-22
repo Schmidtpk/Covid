3474085255c9bef06b99bfb92ec49f005f2a7844
Schmidtp <pschmidte@gmail.com>
2020-03-22 11:39:26 +0100

    add data roadmap (partly to be done)

---
+++ data-raw/data roadmap.R
@@ -0,0 +1,21 @@
+#   Data roadmap
+- each folder contains different data sources
+- package data sets are created in those folders
+- subsequently treatments/measures are merged into the data.sets in merge_italy/merge_world
+
+# measures/create measures.R
+- downloads the google document with measures and does some formating
+- saves data as "measures" in package
+
+# merge_italy.R
+- takes the italy data generated in kaggle italy and merges the measures to it
+- generates for each treatment
+  - column "treatmeant_active": positive, when treatment is active
+  - column "treatmeant_t": stating date of first enactement
+  - column "treatment_diff": specifiying time to/since first enactement (before negative, afterwards positive)
+- new data safed as "it" in package
+
+# merge_world.R
+- takes the international data and merges the measures to it
+- new data safes as "world" in package
+- same rules as in merge_italy.R apply for the new columns
