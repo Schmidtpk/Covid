f4531d61f118f531e465d28dfc1209616bf813d9
Schmidtp <pschmidte@gmail.com>
2020-03-23 00:35:05 +0100

    add show_measures_of()

--- analysis/check individual time series.R
+++ analysis/check individual time series.R
@@ -4,3 +4,14 @@ show_ts(outcome = "log(Deaths)",regions = NULL)+facet_wrap(vars(region))
 
 
 show_ts(df = world, outcome = "dead",regions = "Italy")
+
+show_ts(df = world, outcome = "dead",regions = "Germany")
+
+show_ts(df = world, outcome = "dead",regions = "Mainland China")+
+  facet_wrap(vars(region),scales = "free_y")
+
+
+show_measures_of(c("Germany","Italy"))
+
+
+
