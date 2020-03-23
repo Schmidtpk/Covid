773c1b6d2c1f94b07717072f48ea52ae4d733cb7
Schmidtp <pschmidte@gmail.com>
2020-03-23 10:56:14 +0100

    add keep_units_with() that drops data without measures

---
+++ R/data manipulations.R
@@ -0,0 +1,27 @@
+
+#' Drops any units (countries) that do not have measures imposed
+#'
+#' @param df data frame to be subsetted
+#' @param measures vector of measures to be considered
+#'
+#' @return pdata.frame
+#' @export
+#'
+#' @examples
+#' keep_units_with()
+keep_units_with<-function(df=world, measures=world.measures)
+{
+  df$any_measures <- rowSums(df[paste0(measures,"_active")])
+  df <- df %>% group_by(label)%>%
+    mutate(any_measures_in_label=sum(any_measures)>0) %>%
+    ungroup() %>% pdata.frame(index=c("t","label"))
+
+  message(paste("Drop ",length(unique(df$label[!df$any_measures_in_label])), "units of observation"))
+
+  df <- subset(df,any_measures_in_label)
+  df$any_measures_in_label <- NULL
+  df$any_measures <- NULL
+
+  df
+}
+df <- keep_units_with()
