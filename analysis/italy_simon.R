c52592f64e67ab318c9f8b46467d5b3dbe897e97
Heß <shess@its.uni-frankfurt.de>
2020-03-23 20:02:55 +0100

    update italy analyisis

--- analysis/italy_simon.R
+++ analysis/italy_simon.R
@@ -11,18 +11,17 @@ it$growth<-it$pos.total/lag(it$pos.total)
 it$CurfewImildonlyprivatepubliclifeI_active<-it$CurfewILockdownofAllNonEssentialPublicLifeI_active+it$CurfewImildonlyprivatepubliclifeI_active>0
 
 
-model.plm.iFX<-plm(paste("growth ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(it,subset = pos.total>10) ,effect = "individual")
-summary(model.plm.iFX)
-
 model.plm.pooling<-plm(paste("growth ~", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(it,subset = pos.total>10) ,model = "pooling")
-summary(model.plm.pooling)
+model.plm.iFX<-plm(paste("growth ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(it,subset = pos.total>10) ,effect = "individual")
+model.plm.iFXtrends<-plm(paste("growth ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(RegionCode)*Date"),subset(it,subset = pos.total>10) ,effect = "individual")
 
+rob_se <- list(sqrt(diag(vcovHC(model.plm.iFX, type = "HC1",cluster="group"))),
+               sqrt(diag(vcovHC(model.plm.pooling, type = "HC1",cluster="group"))),
+               sqrt(diag(vcovHC(model.plm.iFXtrends, type = "HC1",cluster="group"))))
 
-
-summary(model.plm.iFX,
-        vcov=vcovHC(model.plm.iFX,cluster="group"))
-summary(model.plm.pooling,
-        vcov=vcovHC(model.plm.pooling,cluster="group"))
-
-
-
+stargazer(model.plm.pooling,
+          model.plm.iFX,
+          model.plm.iFXtrends,
+          se=rob_se,type="text",omit=c("Date$", "^Constant$"),
+          add.lines = list(c("Region fixed effects", "No","Yes", "Yes"),
+                           c("Region-specific time trends","No", "No", "Yes")))
