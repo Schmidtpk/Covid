0ad36679780e8de7c3857b3b6b442597a9eee989
simonheb <simon.h.hess@gmail.com>
2020-03-24 11:33:39 +0100

    buggy

--- analysis/world_simon.R
+++ analysis/world_simon.R
@@ -5,6 +5,7 @@ world.measures
 treatments<-c("UniversityClosings", "SchoolClosings", "CurfewIMildOnlyPrivatePublicLifeI", "CurfewILockdownofAllNonEssentialPublicLifeI")
 
 df <- subset(world, is.na(in_country))
+show_ts(outcome = "pos.total/lag(pos.total)",regions = NULL)+facet_wrap(vars(region))
 
 
 #drop countries without any measures in data set
@@ -12,39 +13,33 @@ df <- keep_units_with(df,treatments)
 
 show_measures_of(df = df,units = unique(df$country))
 
-df$growth<-df$pos.total/lag(df$pos.total)
+df$growth<-df$pos.total/plm::lag(df$pos.total)
+df$lagpos<-plm::lag(df$pos.total)
+View(df[,c("growth","lagpos", "pos.total",names(df))])
+ende
 
 
-#df$CurfewIMildOnlyPrivatePublicLifeI_active<-(df$CurfewIMildOnlyPrivatePublicLifeI_active+df$CurfewILockdownofAllNonEssentialPublicLifeI_active)>0
 
 
+df$datenum<-as.numeric(df$Date)-18315
 
-model.plm.pooling<-plm(paste("growth ~", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,model = "pooling")
-model.plm.iFX<-plm(paste("growth ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,effect = "individual")
-<<<<<<< HEAD
-model.plm.iFXtrends<-plm(paste("growth ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*Date"),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.pooling<-plm(paste("log(growth) ~", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,model = "pooling")
+model.plm.iFX<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.2iFX<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),""),subset(df,subset = pos.total>10) ,effect = "twoway")
+model.plm.iFXtrends<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*datenum"),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.iFXtrends2<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*poly(datenum,2)"),subset(df,subset = pos.total>10) ,effect = "individual")
 
-=======
-model.plm.iFXtrends<-plm(paste("growth ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*as.numeric(t)"),subset(df ,subset = pos.total>10) ,effect = "individual")
-
-rob_se <- list(sqrt(diag(vcovHC(model.plm.iFX, type = "HC1",cluster="group"))),
-               sqrt(diag(vcovHC(model.plm.pooling, type = "HC1",cluster="group"))),
-               sqrt(diag(vcovHC(model.plm.iFXtrends, type = "HC1",cluster="group"))))
-
-stargazer(model.plm.pooling,
-          model.plm.iFX,
-          model.plm.iFXtrends,
-          se=rob_se,type="text",
-          #omit=c("Date$", "^Constant$"),
-          add.lines = list(c("Region fixed effects", "No","Yes", "Yes"),
-                           c("Region-specific time trends","No", "No", "Yes")))
->>>>>>> 201d23ce7c150d80c410d9fe3afe635d3aa04bca
 
 
 stargazer(type="text",
-          omit=c("Date$"),
-          omit.labels=c("Country-specific time trends"),
+          omit=c("datenum$","poly\\(datenum, 2\\)"),
+          omit.labels=c("Country-specific time trends","Country-specific time trends (squared)"),omit.yes.no = c("Yes","-"),
           lmtest::coeftest(model.plm.pooling,vcov=vcovHC(model.plm.pooling,cluster="group")),
           lmtest::coeftest(model.plm.iFX,vcov=vcovHC(model.plm.iFX,cluster="group")),
+          lmtest::coeftest(model.plm.2iFX,vcov=vcovHC(model.plm.2iFX,cluster="group")),
           lmtest::coeftest(model.plm.iFXtrends,vcov=vcovHC(model.plm.iFXtrends,cluster="group")),
-          add.lines = list(c("Country fixed effects", "No","Yes", "Yes")))
+          lmtest::coeftest(model.plm.iFXtrends2,vcov=vcovHC(model.plm.iFXtrends2,cluster="group")),
+          add.lines = list(c("Country fixed effects", "-","Yes", "Yes","Yes", "Yes"),
+                           c("Day fixed effects", "-","-", "Yes","-", "-")
+          ))
+
