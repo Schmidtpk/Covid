0c65596a13c577e7d306f3ff5239b97c06df9e8a
simonheb <simon.h.hess@gmail.com>
2020-03-24 18:44:28 +0100

    analysis update

--- analysis/world_simon.R
+++ analysis/world_simon.R
@@ -5,29 +5,22 @@ world.measures
 treatments<-c("UniversityClosings", "SchoolClosings", "CurfewIMildOnlyPrivatePublicLifeI", "CurfewILockdownofAllNonEssentialPublicLifeI")
 
 df <- subset(world, is.na(in_country))
-show_ts(outcome = "pos.total/lag(pos.total)",regions = NULL)+facet_wrap(vars(region))
-
 
 #drop countries without any measures in data set
-df <- keep_units_with(df,treatments)
+df <- df[units_with(df,treatments),]
 
 show_measures_of(df = df,units = unique(df$country))
 
 df$growth<-df$pos.total/plm::lag(df$pos.total)
 df$lagpos<-plm::lag(df$pos.total)
-View(df[,c("growth","lagpos", "pos.total",names(df))])
-ende
-
-
-
 
 df$datenum<-as.numeric(df$Date)-18315
 
-model.plm.pooling<-plm(paste("log(growth) ~", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,model = "pooling")
-model.plm.iFX<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,effect = "individual")
-model.plm.2iFX<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),""),subset(df,subset = pos.total>10) ,effect = "twoway")
-model.plm.iFXtrends<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*datenum"),subset(df,subset = pos.total>10) ,effect = "individual")
-model.plm.iFXtrends2<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*poly(datenum,2)"),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.pooling<-plm(paste("(growth) ~", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,model = "pooling")
+model.plm.iFX<-plm(paste("(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.2iFX<-plm(paste("(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),""),subset(df,subset = pos.total>10) ,effect = "twoway")
+model.plm.iFXtrends<-plm(paste("(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*datenum"),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.iFXtrends2<-plm(paste("(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*poly(datenum,2)"),subset(df,subset = pos.total>10) ,effect = "individual")
 
 
 
