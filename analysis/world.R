201d23ce7c150d80c410d9fe3afe635d3aa04bca
Schmidtp <pschmidte@gmail.com>
2020-03-24 10:13:12 +0100

    add summary for measures and countryVSmeasures

--- analysis/world.R
+++ analysis/world.R
@@ -1,35 +1,40 @@
 library(Covid)
 
-world.measures
+# summary data ------------------------------------------------------------
 
-treatments<-c("UniversityClosings", "SchoolClosings", "CurfewIMildOnlyPrivatePublicLifeI", "CurfewILockdownofAllNonEssentialPublicLifeI")
+# available treatments
+world.measures
+show_countries_of(measure = c("School","Curfew","Ban","Canc","clos"))
 
 df <- subset(world, is.na(in_country))
 
+dflong <- df %>%
+  tidyr::pivot_longer(cols = matches(world.measures) & ends_with("_active"),
+                      c("measure"),values_to = "active")
+
+measures.summary <-  dflong %>% group_by(measure) %>%
+  summarise(first = min(Date[active]),
+            last = max(Date[active]),
+            ratio_active = mean(active),
+            num_countries = sum(length(unique(country[active]))),
+            enacted_in = list(unique(country[active])))
+measures.summary
+
+measures.country.summary <-  dflong %>% group_by(measure,country) %>%
+  summarise(first = min(Date[active]),
+            last = max(Date[active]),
+            days_active = sum(active),
+            range_active = last-first+1,
+            enacted_in = list(unique(country[active]))) %>% filter(days_active>0)
+View(measures.country.summary)
+
+
+
 
 #drop countries without any measures in data set
-df <- keep_units_with(df,treatments)
+#df <- keep_units_with(df,treatments)
 
-show_measures_of(df = df,units = unique(df$country))
+show_measures_of(df = df)
+show_countries_of(df = df,measure_name = world.measures)
 
-
-df$growth<-df$pos.total/lag(df$pos.total)
-#df <- subset(df,!is.na(growth) & is.finite(growth))
-
-model.plm.iFX<-plm("growth ~ dplyr::between(SchoolClosings_diff,4,6)",
-                   subset(df,subset = pos.total>10),
-                   model = "pooling")
-summary(model.plm.iFX)
-
-
-
-model.plm.iFX<-plm(paste("growth ~ ", paste(paste0("lag(",treatments[2],"_active,5)"),collapse = "+")),
-                   subset(df,subset = pos.total>10) ,
-                   model = "pooling")
-summary(model.plm.iFX)
-
-model.plm.iFX<-plm(paste("growth ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),
-                   subset(df,subset = pos.total>10) ,
-                   model = "pooling")
-summary(model.plm.iFX)
 
