60699b0f4c4362a2b706ab250ef326d6c04f71b8
simonheb <simon.h.hess@gmail.com>
2020-03-25 09:52:38 +0100

    population data

--- data-raw/population from geonames.R
+++ data-raw/population from geonames.R
@@ -1,8 +1,8 @@
 colnames<-c("geonameid","name","asciiname","alternatenames","latitude","longitude","feature class","feature code","country code","cc2","admin1 code","admin2 code","admin3 code","admin4 code","population","elevation","dem","timezone","modification date")
 
-regions<-read.csv("../../../allCountries.txt",sep="\t",col.names=colnames,fill=TRUE)
+regions<-read.csv("../../allCountries.txt",sep="\t",col.names=colnames,fill=TRUE)
 
-regions<-regions[which(ad$feature.class=="A"),]
+regions<-regions[which(regions$feature.class=="A"),]
 regions$name<-NULL
 regions$alternatenames<-NULL
 regions$feature.class<-NULL
@@ -11,4 +11,15 @@ regions$admin2.code<-NULL
 regions$admin3.code<-NULL
 regions$admin4.code<-NULL
 regions$elevation<-NULL
-regions2<-regions[which(regions$feature.code=="ADM1"|regions$feature.code=="PCLI"),]
+
+
+regions<-regions[which(regions$feature.code=="ADM1"|regions$feature.code=="PCLI"),]
+#type_cast
+regions$latitude<-as.numeric(levels(regions$latitude))[regions$latitude]
+regions$longitude<-as.numeric(levels(regions$longitude))[regions$longitude]
+regions$population<-as.numeric(levels(regions$population))[regions$population]
+regions$dem<-as.numeric(levels(regions$dem))[regions$dem]
+regions$geonameid<-as.numeric(levels(regions$geonameid))[regions$geonameid]
+regions$modification.date<-as.Date(levels(regions$modification.date),format="%Y-%m-%d")[regions$modification.date]
+population<-droplevels(regions)
+saveRDS(population,"data/population.RDa")
