5f0d2b5c170dd717c1ec5a67192015fb18c624cd
simonheb <simon.h.hess@gmail.com>
2020-03-25 13:18:05 +0100

    population manual

--- data-raw/population from geonames.R
+++ data-raw/population from geonames.R
@@ -1,27 +1,28 @@
 colnames<-c("geonameid","name","asciiname","alternatenames","latitude","longitude","feature class","feature code","country code","cc2","admin1 code","admin2 code","admin3 code","admin4 code","population","elevation","dem","timezone","modification date")
 
-regions<-read.csv("../../allCountries.txt",sep="\t",col.names=colnames,fill=TRUE)
+##adm1 dataset for coding instructions
+sss<-read.csv("data-raw/admin1CodesASCII.txt",sep="\t",encoding="UTF-8",col.names = c("code","name","name_ascii","id"))
+write.csv(sss,"data/adm1_codes.csv")
 
-regions<-regions[which(regions$feature.class=="A"),]
-regions$name<-NULL
-regions$alternatenames<-NULL
-regions$feature.class<-NULL
-regions$cc2<-NULL
-regions$admin2.code<-NULL
-regions$admin3.code<-NULL
-regions$admin4.code<-NULL
-regions$elevation<-NULL
-
-
-regions<-regions[which(regions$feature.code=="ADM1"|regions$feature.code=="PCLI"),]
-#type_cast
-regions$latitude<-as.numeric(levels(regions$latitude))[regions$latitude]
-regions$longitude<-as.numeric(levels(regions$longitude))[regions$longitude]
-regions$population<-as.numeric(levels(regions$population))[regions$population]
-regions$dem<-as.numeric(levels(regions$dem))[regions$dem]
-regions$geonameid<-as.numeric(levels(regions$geonameid))[regions$geonameid]
-regions$modification.date<-as.Date(levels(regions$modification.date),format="%Y-%m-%d")[regions$modification.date]
-population<-droplevels(regions)
+population<-NULL
+for(country in unique(substr(sss$code,1,2))) {
+  temp <- tempfile()
+  download.file(paste0("https://download.geonames.org/export/dump/",country,".zip"),temp)
+  unzip(zipfile=temp,exdir="data-raw/geonames")
+  regions<-read.csv(paste0("data-raw/geonames/",country, ".txt"),col.names=colnames,sep="\t")
+  regions<-regions[which(regions$feature.code=="ADM1"|regions$feature.code=="PCLI"),]
+  regions<-regions[which(regions$feature.class=="A"),]
+  regions$name<-NULL
+  regions$alternatenames<-NULL
+  regions$feature.class<-NULL
+  regions$cc2<-NULL
+  regions$admin2.code<-NULL
+  regions$admin3.code<-NULL
+  regions$admin4.code<-NULL
+  regions$elevation<-NULL
+  regions$modification.date<-as.Date(levels(regions$modification.date),format="%Y-%m-%d")[regions$modification.date]
+  regions<-droplevels(regions)
+  population<-rbind(regions,population)
+}
 saveRDS(population,"data/pop.rda")
-adm1codes<-population[which(population$feature.code=="ADM1"),c("country.code","asciiname","admin1.code")]
-write.csv(adm1codes,"data/adm1codes.csv")
+
