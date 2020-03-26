43d25a5e55bd3f3ba77054cff79d20fb63115c70
simonheb <simon.h.hess@gmail.com>
2020-03-26 18:25:36 +0100

    analysis

--- analysis/italy_simon.R
+++ analysis/italy_simon.R
@@ -1,10 +1,10 @@
 library(Covid)
 
-show_ts(outcome = "pos.total/lag(pos.total)",regions = NULL)+facet_wrap(vars(region))
+show_ts(outcome = "pos.total/plm::lag(pos.total)",regions = NULL)+facet_wrap(vars(region))
 
 show_ts(outcome = "pos.total",regions = NULL)+facet_wrap(vars(region))
 
-treatments<-c("UniversityClosings", "SchoolClosings", "CurfewImildonlyprivatepubliclifeI", "CurfewILockdownofAllNonEssentialPublicLifeI")
+treatments<-c("UniversityClosings", "SchoolClosings", "CurfewIMildOnlyPrivatePublicLifeI", "CurfewILockdownofAllNonEssentialPublicLifeI")
 
 it$growth<-it$pos.total/lag(it$pos.total)
 
@@ -12,7 +12,7 @@ it$datenum<-as.numeric(it$Date)-18315
 
 model.plm.pooling<-plm(paste("log(growth) ~", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(it,subset = pos.total>10) ,model = "pooling")
 model.plm.iFX<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(it,subset = pos.total>10) ,effect = "individual")
-model.plm.2iFX<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),""),subset(it,subset = pos.total>10) ,effect = "twoway")
+#model.plm.2iFX<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),""),subset(it,subset = pos.total>10) ,effect = "twoway")
 model.plm.iFXtrends<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(RegionCode)*datenum"),subset(it,subset = pos.total>10) ,effect = "individual")
 model.plm.iFXtrends2<-plm(paste("log(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(RegionCode)*poly(datenum,2)"),subset(it,subset = pos.total>10) ,effect = "individual")
 
@@ -26,10 +26,10 @@ stargazer(type="text",
           omit.labels=c("Region-specific time trends","Region-specific time trends (squared)"),omit.yes.no = c("Yes","-"),
           lmtest::coeftest(model.plm.pooling,vcov=vcovHC(model.plm.pooling,cluster="group")),
           lmtest::coeftest(model.plm.iFX,vcov=vcovHC(model.plm.iFX,cluster="group")),
-          lmtest::coeftest(model.plm.2iFX,vcov=vcovHC(model.plm.2iFX,cluster="group")),
+#          lmtest::coeftest(model.plm.2iFX,vcov=vcovHC(model.plm.2iFX,cluster="group")),
           lmtest::coeftest(model.plm.iFXtrends,vcov=vcovHC(model.plm.iFXtrends,cluster="group")),
           lmtest::coeftest(model.plm.iFXtrends2,vcov=vcovHC(model.plm.iFXtrends2,cluster="group")),
-          add.lines = list(c("Region fixed effects", "-","Yes", "Yes","Yes", "Yes"),
-                           c("Day fixed effects", "-","-", "Yes","-", "-")
+          add.lines = list(c("Region fixed effects", "-","Yes", "Yes", "Yes")#,
+#                           c("Day fixed effects", "-","-", ""-", "-")
           ))
 
--- analysis/world_simon.R
+++ analysis/world_simon.R
@@ -1,38 +1,65 @@
 library(Covid)
+library(EpiEstim)
 
 world.measures
 
-treatments<-c("UniversityClosings", "SchoolClosings", "CurfewIMildOnlyPrivatePublicLifeI", "CurfewILockdownofAllNonEssentialPublicLifeI")
+treatments<-c("SchoolClosings","CurfewIMildOnlyPrivatePublicLife")
+# subset only countries (no regions)
+df <- subset(Covid::all, adm_level==1)
+df$`share_RestaurantBarClosure`<-df$`share_Restaurant/BarClosure`
 
-df <- subset(world, is.na(in_country))
 
-#drop countries without any measures in data set
-df <- df[units_with(df,treatments),]
-
-show_measures_of(df = df,units = unique(df$country))
+df$pos.total<-df$pos.new
+for(i in 1:100)
+  df$pos.total<-df$pos.new+ifelse(is.na(plm::lag(df$pos.total)),0,plm::lag(df$pos.total))
 
 df$growth<-df$pos.total/plm::lag(df$pos.total)
-df$lagpos<-plm::lag(df$pos.total)
+df$growth[which(df$pos.total<10)]<-NA
 
-df$datenum<-as.numeric(df$Date)-18315
+df$R0_alt<-NA
+for (ccc in unique(df$country)) {
+  currdf<-which(df$country==ccc)
+  if(length(currdf)<=2)
+    next;
+  t_start <- seq(2, length(df[currdf,c("pos.new")])-1)
+  t_end <- t_start + 1
+  incid<-c(df[currdf,c("pos.new")])
+  incid[is.na(incid)]<-0
+  Rres <- estimate_R(
+    incid = incid,
+    method = "parametric_si",
+    config = make_config(list(
+      t_start = t_start, t_end = t_end,
+      mean_si = 5, std_si = 3
+    )))
+  df[currdf,c("R0_alt")]<-c(NA,NA,c(Rres$R$`Mean(R)`))
+}
 
-model.plm.pooling<-plm(paste("(growth) ~", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,model = "pooling")
-model.plm.iFX<-plm(paste("(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+")),subset(df,subset = pos.total>10) ,effect = "individual")
-model.plm.2iFX<-plm(paste("(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),""),subset(df,subset = pos.total>10) ,effect = "twoway")
-model.plm.iFXtrends<-plm(paste("(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*datenum"),subset(df,subset = pos.total>10) ,effect = "individual")
-model.plm.iFXtrends2<-plm(paste("(growth) ~ ", paste(paste0("lag(",treatments,"_active,5)"),collapse = "+"),"+factor(country)*poly(datenum,2)"),subset(df,subset = pos.total>10) ,effect = "individual")
 
-
+model.plm.pool<-plm(paste("log(growth) ~ ", paste(paste0("plm::lag(share_",treatments,",6)"),collapse = "+")),subset(df,subset = pos.total>10) ,model = "pooling")
+model.plm.iFX<-plm(paste("log(growth) ~ ", paste(paste0("plm::lag(share_",treatments,",6)"),collapse = "+")),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.iFXtrends<-plm(paste("log(growth) ~ ", paste(paste0("plm::lag(share_",treatments,",6)"),collapse = "+"),"+factor(country)*date"),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.iFXtrends2<-plm(paste("log(growth) ~ ", paste(paste0("plm::lag(share_",treatments,",6)"),collapse = "+"),"+factor(country)*poly(date,2)"),subset(df,subset = pos.total>10) ,effect = "individual")
 
 stargazer(type="text",
-          omit=c("datenum$","poly\\(datenum, 2\\)"),
-          omit.labels=c("Country-specific time trends","Country-specific time trends (squared)"),omit.yes.no = c("Yes","-"),
-          lmtest::coeftest(model.plm.pooling,vcov=vcovHC(model.plm.pooling,cluster="group")),
+          omit=c("date\\d*$","poly\\(date"),
+          omit.labels=c("Country-specific time trend","Country-specific squared time trend"),omit.yes.no = c("Yes","-"),
           lmtest::coeftest(model.plm.iFX,vcov=vcovHC(model.plm.iFX,cluster="group")),
-          lmtest::coeftest(model.plm.2iFX,vcov=vcovHC(model.plm.2iFX,cluster="group")),
           lmtest::coeftest(model.plm.iFXtrends,vcov=vcovHC(model.plm.iFXtrends,cluster="group")),
-          lmtest::coeftest(model.plm.iFXtrends2,vcov=vcovHC(model.plm.iFXtrends2,cluster="group")),
-          add.lines = list(c("Country fixed effects", "-","Yes", "Yes","Yes", "Yes"),
-                           c("Day fixed effects", "-","-", "Yes","-", "-")
-          ))
+          lmtest::coeftest(model.plm.iFXtrends2,vcov=vcovHC(model.plm.iFXtrends2,cluster="group"))
+          )
+
+
+model.plm.pool<-plm(paste("R0_alt ~ ", paste(paste0("plm::lag(share_",treatments,",6)"),collapse = "+")),subset(df,subset = pos.total>10) ,model = "pooling")
+model.plm.iFX<-plm(paste("R0_alt ~ ", paste(paste0("plm::lag(share_",treatments,",6)"),collapse = "+")),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.iFXtrends<-plm(paste("R0_alt ~ ", paste(paste0("plm::lag(share_",treatments,",6)"),collapse = "+"),"+factor(country)*date"),subset(df,subset = pos.total>10) ,effect = "individual")
+model.plm.iFXtrends2<-plm(paste("R0_alt ~ ", paste(paste0("plm::lag(share_",treatments,",6)"),collapse = "+"),"+factor(country)*poly(date,2)"),subset(df,subset = pos.total>10) ,effect = "individual")
+
+stargazer(type="text",
+          omit=c("date\\d*$","poly\\(date"),
+          omit.labels=c("Country-specific time trend","Country-specific squared time trend"),omit.yes.no = c("Yes","-"),
+          lmtest::coeftest(model.plm.iFX,vcov=vcovHC(model.plm.iFX,cluster="group")),
+          lmtest::coeftest(model.plm.iFXtrends,vcov=vcovHC(model.plm.iFXtrends,cluster="group")),
+          lmtest::coeftest(model.plm.iFXtrends2,vcov=vcovHC(model.plm.iFXtrends2,cluster="group"))
+          )
 
